ec4525b90bd718788348cc31a64965f269bf4625
Ernaldo Becerra <ernaldito12@gmail.com>
2025-01-13 22:21:29 -0800

    “MESSAGE_HERE”

--- 1-variables.js
+++ 1-variables.js
@@ -2,8 +2,8 @@
 let cajaDeAldo = "Mono"
 // Create variables for your name, age, and whether you like pizza
 let name = "Ernaldo"
-let age = "27"
-let likesPizza = 'true'
+let age = 27
+let likesPizza = true
 // Print all variables to the console
 console.log(cajaDeAldo)
 console.log(name)
