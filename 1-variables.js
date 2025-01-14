c6d2decc1714f0e2cb249cc38e499d54122e22e9
Ernaldo Becerra <ernaldito12@gmail.com>
2025-01-13 17:31:17 -0800

    task done

--- 1-variables.js
+++ 1-variables.js
@@ -1,3 +1,11 @@
 // Exercise 1: Basic Variable Declaration
+let cajaDeAldo = "Mono"
 // Create variables for your name, age, and whether you like pizza
+let name = "Ernaldo"
+let age = "27"
+let likesPizza = 'true'
 // Print all variables to the console
+console.log(cajaDeAldo)
+console.log(name)
+console.log(age)
+console.log(likesPizza)
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # javascript
+
+Get latest code
+git pull
+
+Create a pull request
+git checkout -b branch_name
+git add .
+git commit -m "MESSAGE_HERE"
+git push -f
+    link appears in terminal -> copy/paste en browser
+    create pull request in browser
